4774de0e38817a339d7da181c7a95108603ba6ef
L12015 <rosemble@lotte.net>
2015-02-05 18:42:53 +0900

    1.1

--- QnDQ/src/Deque.java
+++ QnDQ/src/Deque.java
@@ -1,35 +1,80 @@
 import java.util.Iterator;
 
-
-
-
 public class Deque<Item> implements Iterable<Item> {
 	
-	private nodeFirst 
+	
+	private int N;
+	private Node first;
+	private Node last;
+	
+    // helper linked list class
+    private class Node {
+        private Item item;
+        private Node next;
+    }
+    
+    
 	public Deque()                           // construct an empty deque
 	{
+		first = null;
+		last = null;
+		N = 0;
 		
 	}
 	public boolean isEmpty()                 // is the deque empty?
 	{
-		return false;
+		return first == null;
 	}
+	
 	public int size()                        // return the number of items on the deque
 	{
-		return 0;
+		return N;
 	}
 	public void addFirst(Item item)          // insert the item at the front
 	{
 		if (item == null) throw new java.lang.NullPointerException();
 		
+		Node oldFirst = first;
+		first = new Node();
 		
+		first.item = item;
+		first.next = null;
+		
+		if ( isEmpty())
+			last = first;
+		else 
+			first.next = oldFirst;
+		N++;
 	}
-	public void addLast(Item item)           // insert the item at the end
+	
+	public void addLast(Item item)          // insert the item at the end
 	{
 		if (item == null) throw new java.lang.NullPointerException();
 		
+		Node oldFirst = first;
+		first = new Node();
 		
+		first.item = item;
+		first.next = null;
+		
+		if ( isEmpty())
+			last = first;
+		else 
+			first.next = oldFirst;
+		N++;
 	}
+	
+	public void enqueue(Item item) {
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else           oldlast.next = last;
+        N++;
+        //assert check();
+    }
+	
 	public Item removeFirst()                // delete and return the item at the front
 	{
 		return null;
